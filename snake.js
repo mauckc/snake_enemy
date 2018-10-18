878ebe764f176529f01f655e6556ef445464d845
Ross Mauck <ross@rossmauck.com>
2018-10-18 16:54:28 -0700

    Snake has achieved specialness (supermode)
    there were times of sorrow, times of hollow, times of following, times of death and destruction...

--- snake.js
+++ snake.js
@@ -6,8 +6,10 @@ class Snake {
     this.xdir = 0;
     this.ydir = 0;
     this.len = 0;
-    this.color = color("#0e0eba");
+    this.headColor = color("#0e0eba");
+    this.bodyColor = color("#0e0eba");
     this.hunger = 0;
+    this.superTimer = 250;
   }
   
   setDir(x, y) {
@@ -21,6 +23,7 @@ class Snake {
     head.x += this.xdir;
     head.y += this.ydir;
     this.body.push(head);
+    this.superTimer--;
     
     // Periodic Boundary Conditions
     if (head.x>=w)
@@ -63,42 +66,72 @@ class Snake {
     //if ( (enemyxpos - 10 < x && x < enemyxpos + 20) && (enemyypos - 10 < y && y < enemyypos+20) ) {
     //   return true;
     //}
-    for(let i = 0; i < this.body.length-1; i++) {
+
+
+    
+
+    for(let i = 0; i <= this.body.length-1; i++) {
     	let part = this.body[i];
       if((enemyxpos - 10 < part.x && part.x < enemyxpos + 20) && (enemyypos - 10 < part.y && part.y < enemyypos + 20)) {
-      	return true;
+        //if super mode is on, then head (and only head) of snake will be able to kill enemy.
+        if(this.superTimer > 0 && i == this.bodyLength - 1) {
+          //send message to KILL enemy.
+          
+          return false;
+        }
+        else return true;
       }
     }
     return false;
   }
   
-  eat(pos) {
+  eat(food) {
   	let x = this.body[this.body.length-1].x;
     let y = this.body[this.body.length-1].y;
   
-    if((pos.x <= x && x <= pos.x + foodxsize) && (pos.y <= y && y <= pos.y + foodysize)) {
+    if((food.x <= x && x <= food.x + foodxsize) && (food.y <= y && y <= food.y + foodysize)) {
+      if(food.z == 1){this.activateSuperMode()}
       this.grow();
       this.hunger = 0;
       return true;
     }
     // Snake gets hungrier everytime it tries to eat and fails
+    if(this.superTimer <= 0){
     this.hunger++;
-     if (this.hunger>score.difficulty){
+    }
+    if (this.hunger>score.difficulty){
     end = true;
-  }
+    }
     return false;
   }
   
+  activateSuperMode() {
+    this.superTimer = 250;
+    this.headColor = color("#0ebaba");
+
+
+  }
+
   show() {
     noStroke();
-    fill(this.color);
+    fill(this.headColor);
     // Make text shake with merlin noise with amplitude directly related
     //  to the snake's hunger
-    this.hungriness = noise(this.hunger)*10
+    if(this.hunger < score.difficulty/2)
+    {this.hungriness = noise(0);}
+    else{this.hungriness = noise(this.hunger)*10}
     
-    text("snake", this.body[0].x  + 18 +this.hungriness, this.body[0].y + 20+this.hungriness, 50, 50);
-    text(this.hunger, this.body[0].x + 20+this.hungriness, this.body[0].y - 20+this.hungriness, 50, 50);
-  	for(let i = 0; i < this.body.length; i++) {
+    text("snake", this.body[0].x  + 18, this.body[0].y + 20, 50, 50);
+    if(this.superTimer <= 0){
+      this.headColor = color("#0e0eba");
+      text(this.hunger, this.body[0].x + 20+this.hungriness, this.body[0].y - 20+this.hungriness, 50, 50);
+    }
+    else{text(this.superTimer, this.body[0].x + 20, this.body[0].y - 20, 50, 50);}
+
+    rect(this.body[this.body.length - 1].x, this.body[this.body.length - 1].y, 10, 10);
+    
+    fill(this.bodyColor);
+  	for(let i = 0; i < this.body.length - 1; i++) {
       rect(this.body[i].x, this.body[i].y, 10, 10);
     }
   }
